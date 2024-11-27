15c5d80f50f51e040ba1017a3474edba1d680881
XIAOBAI-creative <victorwzhang1@gmail.com>
2024-11-26 21:36:04 -0800

    Update tree.hpp

--- tree.hpp
+++ tree.hpp
@@ -50,7 +50,7 @@ class BinaryTreeIterator : public std::input_iterator_tag
     // actually start the traversal
    BinaryTreeIterator(BinaryTreeNode<K, V> *root, bool start)
 {
-    if (start && root != nullptr)
+    if (start && root != nullptr)//start by most left node if iterator point smallest(left) element and root node not empty
     {
         pushLeft(root);
         if (!working_stack.empty())
@@ -77,7 +77,8 @@ public:
     // still more work to do...
     bool operator!=(const BinaryTreeIterator<K, V> &other) const
     {
-        return current != other.current;
+        return current != other.current;//check if iterator continue or not
+
     }
 
     // This is the heart of the tree traversal algorithm.
@@ -111,6 +112,12 @@ public:
     }
 
     // And this visits the node itself...
+// In visiting the node, it should 
+    // both set current to be the current node's right
+    // and then return a pair constructed from the old 
+    // current node's key and value.
+// This isn't actually what you want to return,
+        // its just a placeholder so the compiler doesn't complain.
     std::pair<K, V> operator*() const
     {
         return std::pair<K, V>(current->key, current->value);
@@ -118,7 +125,8 @@ public:
 
 private:
     void pushLeft(BinaryTreeNode<K, V> *node)
-{
+{//node and its left node push to stack
+        
     while (node != nullptr)
     {
         working_stack.push(node);
@@ -143,9 +151,21 @@ public:
     BinaryTree() : root(nullptr)
     {
     }
+// The [] operation is for both getting and setting.
+    // If the key exists in the tree a reference to the
+    // associated value is returned.  Otherwise, it will
+    // create a new tree node and return that value.
 
+    // What you should do is first check if the root is null
+    // and, if so, just create a new node.
+    // then simply invoke root->find(key) and return
+    // that value as find is already set up to return
+    // a reference.
    V& operator[](const K& key)
-{
+{// THis is just to keep the compiler happy
+        // so your code compiles, this is not what you
+        // actually want to return
+         
     if (root == nullptr)
     {
         root = new BinaryTreeNode<K, V>(key);
@@ -159,7 +179,8 @@ public:
     }
 }
 
-
+   // This should return false if the tree
+    // is empty, otherwise return root->contains
     bool contains(const K &key)
     {
         if (root == nullptr)
@@ -171,7 +192,9 @@ public:
             return root->contains(key);
         }
     }
-
+ // Erases a node if a key matches.  If the
+    // key does not match it simply is an operation
+    // that does nothing.
     void erase(const K &key)
     {
         if (root != nullptr)
@@ -179,7 +202,11 @@ public:
             root = root->erase(key);
         }
     }
-
+ // And the destructor for the binary tree.
+    // In order to prevent memory leaks and keep with
+    // the C++ "RAII" convention, it should see
+    // if there is a root and, if so, call freetree()
+    // on the root.
     ~BinaryTree()
     {
         if (root != nullptr)
@@ -188,7 +215,7 @@ public:
             root = nullptr;
         }
     }
-
+// This returns the iterators.
     BinaryTreeIterator<K, V> begin()
     {
         return BinaryTreeIterator<K, V>(root, true);
@@ -209,11 +236,19 @@ class BinaryTreeNode
     friend class BinaryTree<K, V>;
     friend class BinaryTreeIterator<K, V>;
 
-public:
+public:// The constructor, it simply setts the key and the left/right pointers.
+    // Data defaults to whatever the default value is for the data type.
+  
     BinaryTreeNode(const K &keyin) : key(keyin), left(nullptr), right(nullptr), height(1)
     {
     }
+  // This should recursively free the tree.
+    // It should call freetree on left and 
+    // right and then, as the last act,
+    // call delete on this.  
 
+    // Yes, you can "suicide" an object in C++,
+    // and this is a case where you want to do it.
     void freetree()
     {
         if (left != nullptr)
@@ -230,17 +265,17 @@ public:
     }
 
 protected:
-    int getHeight(BinaryTreeNode<K, V> *node)
+    int getHeight(BinaryTreeNode<K, V> *node)/if node not nullptr then return height, if not exist return 0
     {
         return node ? node->height : 0;
     }
 
     int getBalance()
     {
-        return getHeight(left) - getHeight(right);
+        return getHeight(left) - getHeight(right);//just left - right to see balance
     }
 
-    // 左旋
+    // left rotate
     BinaryTreeNode<K, V> *leftRotate()
     {
         BinaryTreeNode<K, V> *y = right;
@@ -255,7 +290,7 @@ protected:
         return y;
     }
 
-    // 右旋
+    // right rotate
     BinaryTreeNode<K, V> *rightRotate()
     {
         BinaryTreeNode<K, V> *x = left;
@@ -270,16 +305,42 @@ protected:
         return x;
     }
 
-    BinaryTreeNode<K, V> *minValueNode()
+    BinaryTreeNode<K, V> *minValueNode()//follow left node go down until leftest
     {
         BinaryTreeNode<K, V> *current = this;
         while (current->left != nullptr)
             current = current->left;
         return current;
     }
+ // Removing a node from a binary tree, returning
+    // a pointer to the now modified tree.
 
+    // If you aren't at the node you are looking for, just set
+    // your new left or right pointer (as approriate, and if it exists) 
+    // to the  result of recursively calling erase, and then return this.
+
+    // If this node does match the key however, check the
+    // child nodes.  If there is no left, assign the right node to a temporary
+    // value, delete this, and return the right node.
+    // If there is no right node, do the same thing.
+    // If there is both a left and right node, check if the left node
+    // doesn't have a right node of its own.  If so, set that node's right
+    // to the current right, assign left to a temporary, and delete this and
+    // return that left node.
+
+    // Finally, if the left node DOES have a right node, keep
+    // following the right hand side of that subtree until you
+    // find a node without a right hand branch.  In that case set
+    // the node above it's right to that node's left, assign that
+    // node to a temporary, have its left point to the current node's left
+    // its right to the current node's right, delete this and return that
+    // node.
    BinaryTreeNode<K, V>* erase(const K& k)
-{
+{//check k and key is small then in left, vise versa
+       //if equal, then go right tree if node do not have left tree and vise versa then delete current node,
+        // if have both left and right and we use lfet side biggest node to instead(right side smallesr also work i think) then if the node we pick do not have right tree then
+        //connect currevt node to left node right side and return left node as new tree root node, if has right tree, then use left side right node to instead current node, connect to make node left point to lest, right point to right tree then detele current node
+      
     if (k < key)
     {
         if (left != nullptr)
@@ -298,59 +359,54 @@ protected:
 
             if (temp == nullptr)
             {
-                // 无子节点
+              
                 delete this;
                 return nullptr;
             }
             else
             {
-                // 有一个子节点
                 *this = *temp;
                 delete temp;
             }
         }
         else
         {
-            // 有两个子节点
+      
             BinaryTreeNode<K, V>* temp = right->minValueNode();
             key = temp->key;
             value = temp->value;
             right = right->erase(temp->key);
         }
     }
-
-
-
-    // 更新高度
     height = 1 + std::max(getHeight(left), getHeight(right));
-
-    // 获取平衡因子
     int balance = getBalance();
-
-    // 平衡调整
     if (balance > 1 && left && left->getBalance() >= 0)
         return rightRotate();
-
     if (balance > 1 && left && left->getBalance() < 0)
-    {
-        left = left->leftRotate();
+    { left = left->leftRotate();
         return rightRotate();
     }
-
     if (balance < -1 && right && right->getBalance() <= 0)
         return leftRotate();
-
     if (balance < -1 && right && right->getBalance() > 0)
-    {
-        right = right->rightRotate();
+    {   right = right->rightRotate();
         return leftRotate();
-    }
-
-    return this;
+    }return this;
 }
-
+// This seeks to recursively find a key, creating
+    // a new node if necessary.
+    // If k == this node's key, return the value.
+    // If k < this key it is to the left.
+    // If there is no left node, create it with k as the key.
+    // Then recursively return find on the left.  Similar for
+    // the right. 
+    // THis is just to keep the compiler happy
+        // so your code compiles, this is not what you
+        // actually want to return
   BinaryTreeNode<K, V>* find(const K& k, BinaryTreeNode<K, V>*& foundNode)
-{
+{ //ccheck k bigger or smaller then key, then to go left/right tree, if null then ky not in tree, and locate at current node left/right sidde
+    //so cteate new binarytreenode with key k snd use it as current node's left/right node, baisclly just a recursion
+
     if (k == key)
     {
         foundNode = this;
@@ -380,40 +436,27 @@ protected:
             right = right->find(k, foundNode);
         }
     }
-
-    // 更新高度
     height = 1 + std::max(getHeight(left), getHeight(right));
-
-    // 获取平衡因子
     int balance = getBalance();
-
-    // 左左情况
     if (balance > 1 && left && k < left->key)
         return rightRotate();
-
-    // 左右情况
     if (balance > 1 && left && k > left->key)
-    {
-        left = left->leftRotate();
+    {  left = left->leftRotate();
         return rightRotate();
     }
-
-    // 右右情况
     if (balance < -1 && right && k > right->key)
         return leftRotate();
-
-    // 右左情况
     if (balance < -1 && right && k < right->key)
-    {
-        right = right->rightRotate();
+    { right = right->rightRotate();
         return leftRotate();
-    }
-
-    return this;
+    } return this;
 }
 
+   // And contains is a recursive search that doesn't
+    // create new nodes, just checks if the key exists.
     bool contains(const K &k)
-    {
+    {//use recursion to compare key and the key we want to determine way to search, to see if the key we want in the tree or not
+      
         if (k == key)
         {
             return true;
